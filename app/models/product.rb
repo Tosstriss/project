e6fd334942ccc8678250aa1ae5acc96bc0967e45
Joshua Sadiua <joshuasadiua@gmail.com>
2017-11-23 02:29:27 +0000

    Added the best ms paint pictures to all the of my projects

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,9 @@ class Product < ApplicationRecord
 	validates :price, numericality: true
 
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-	#validates_attachment :image, :presence => true,
-  #	:content_type => { :content_type => "image/jpg" },
-  #	:size => { :in => 0..100.kilobytes }
+	validates_attachment :image, :presence => true,
+  	:content_type => { :content_type => /\Aimage\/.*\z/ },
+  	:size => { :in => 0..1000.kilobytes }
 
 
 	belongs_to :category
